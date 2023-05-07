690912771ebf4361b1c41f070789c32dc5aea4fb
Pryta Rosela <132034419+prytarosela@users.noreply.github.com>
2023-05-07 15:05:41 +0700

    Update main.c
    sistem untuk mendaftarkan atau sistem registrasi pengguna

--- main.c
+++ main.c
@@ -3,6 +3,48 @@
 #include <string.h>
 #include <stdbool.h>
 
+typedef struct
+{
+    char nama_user[20];
+    char kata_sandi[20];
+} User;
+
+void daftar();
+User *cariUser(char *nama_user);
+User loginUser(char *nama_user, char *kata_sandi);
+void startKuis();
+int main(int argc, char *argv[]);
+
+void printHeader()
+{
+    printf("**************************************************\n");
+    printf("*                                                *\n");
+    printf("*   Selamat datang di game Who Wants to Be a     *\n");
+    printf("*   Millionaire!                                 *\n");
+    printf("*                                                *\n");
+    printf("**************************************************\n\n");
+}
+void daftar(char *nama_user, char *kata_sandi)
+{
+    User userBaru;
+    FILE *fp;
+
+    strcpy(userBaru.nama_user, nama_user);
+    strcpy(userBaru.kata_sandi, kata_sandi);
+
+    fp = fopen("database/login.bin", "ab");
+    if (fp == NULL)
+    {
+        printf("Error membuka file!\n");
+        exit(1);
+    }
+
+    fwrite(&userBaru, sizeof(User), 1, fp);
+    fclose(fp);
+
+    printf("User berhasil didaftarkan!\n");
+}
+
 
 User *cariUser(char *nama_user)
 {
